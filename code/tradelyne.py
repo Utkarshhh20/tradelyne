05cad78ba4529557fd43573e88df968a82e09127
Utkarsh Gupta <88526325+Utkarshhh20@users.noreply.github.com>
2022-07-29 13:25:54 +0400

    Update tradelyne.py

--- code/tradelyne.py
+++ code/tradelyne.py
@@ -8,7 +8,6 @@ import pandas_datareader as pdr
 import mplfinance as fplt
 import backtrader as bt 
 import matplotlib.pyplot as plt
-import talib
 import matplotlib
 import requests
 import tweepy
@@ -21,7 +20,44 @@ from string import Template
 from datetime import date, timedelta
 from yahoo_fin import stock_info as si 
 from st_aggrid import GridOptionsBuilder, AgGrid, GridUpdateMode, DataReturnMode
+import requests
+import os
+import sys
+# check if the library folder already exists, to avoid building everytime you load the pahe
+if not os.path.isdir("/tmp/ta-lib"):
 
+    # Download ta-lib to disk
+    with open("/tmp/ta-lib-0.4.0-src.tar.gz", "wb") as file:
+        response = requests.get(
+            "http://prdownloads.sourceforge.net/ta-lib/ta-lib-0.4.0-src.tar.gz"
+        )
+        file.write(response.content)
+    # get our current dir, to configure it back again. Just house keeping
+    default_cwd = os.getcwd()
+    os.chdir("/tmp")
+    # untar
+    os.system("tar -zxvf ta-lib-0.4.0-src.tar.gz")
+    os.chdir("/tmp/ta-lib")
+    # build
+    os.system("./configure --prefix=/home/appuser")
+    os.system("make")
+    # install
+    os.system("make install")
+    # install python package
+    os.system(
+        'pip3 install --global-option=build_ext --global-option="-L/home/appuser/lib/" --global-option="-I/home/appuser/include/" ta-lib'
+    )
+    # back to the cwd
+    os.chdir(default_cwd)
+    print(os.getcwd())
+    sys.stdout.flush()
+
+# add the library to our current environment
+from ctypes import *
+
+lib = CDLL("/home/appuser/lib/libta_lib.so.0")
+# import library
+import talib
 st.set_page_config(page_title='Tradelyne', layout="wide",initial_sidebar_state='collapsed')
 today=date.today()
 oneyr= today - timedelta(days=365)
