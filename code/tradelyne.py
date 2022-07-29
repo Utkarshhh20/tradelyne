33ce50a72e42a0d01327e3a85c0b28c5ab101e31
Utkarsh Gupta <88526325+Utkarshhh20@users.noreply.github.com>
2022-07-29 19:24:33 +0400

    Update tradelyne.py

--- code/tradelyne.py
+++ code/tradelyne.py
@@ -1139,7 +1139,6 @@ elif dashboard=='Screener':
             st.header("Stock News Sentiment Analyzer")
             try:
                 st.subheader("Hourly and Daily Sentiment of {} Stock".format(tickerSymbol))
-	        st.write('Hi')
                 news_table = news_headlines(tickerSymbol)
                 parsed_news_df = parse_news(news_table)
                 parsed_and_scored_news = score_news(parsed_news_df)
