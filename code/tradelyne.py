b06e6a273ce666050bd1dfeebbf857de0d66adb7
Utkarsh Gupta <88526325+Utkarshhh20@users.noreply.github.com>
2022-07-30 10:29:16 +0400

    Update tradelyne.py

--- code/tradelyne.py
+++ code/tradelyne.py
@@ -77,7 +77,7 @@ finally:
 st.set_page_config(page_title='Tradelyne', layout="wide",initial_sidebar_state='collapsed')
 st.markdown(""" <style> 
                 .css-18e3th9 {  padding-top: 0rem; 
-                                padding-bottom: 10rem; 
+                                padding-bottom: 5rem; 
                                 padding-left: 0rem; 
                                 padding-right: 0rem; } 
                 .css-1d391kg {  padding-top: 3.5rem; 
