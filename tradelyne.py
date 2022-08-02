8a62e6d83709875d43ac4771033c25200ba6eef6
Utkarshhh20 <88526325+Utkarshhh20@users.noreply.github.com>
2022-08-02 12:06:28 +0400

    Update tradelyne.py

--- tradelyne.py
+++ tradelyne.py
@@ -585,7 +585,7 @@ if dashboard=='Tradelyne':
         }
         </style>
         <body>
-	<center><img src='https://i.postimg.cc/fTtX8hfv/Finance-14-removebg-preview.png' alt='logo' class='logo'></img></center> 
+	<center><a href="https://freeimage.host/i/Sg3eDJ"><img src="https://iili.io/Sg3eDJ.png" alt="Sg3eDJ.png" class ='logo' border="0"></img></a></center> 
         </body>
         '''
 
