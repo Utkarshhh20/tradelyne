5629c9a861811b5d2da75470202c8d7936582b9a
Utkarshhh20 <88526325+Utkarshhh20@users.noreply.github.com>
2022-08-17 23:31:14 +0400

    Update tradelyne.py

--- tradelyne.py
+++ tradelyne.py
@@ -75,6 +75,31 @@ import talib
 #    subprocess.check_call([sys.executable, "-m", "pip", "install", "--global-option=build_ext", "--global-option=-L/home/appuser/lib/", "--global-option=-I/home/appuser/include/", "ta-lib"])
 #finally:
 #    import talib
+anlytcs_code = """<script async src="https://www.googletagmanager.com/gtag/js?id=G-Q38PK1T9RE"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', 'G-Q38PK1T9RE');
+</script>"""
+
+# Fetch the path of the index.html file
+path_ind = os.path.dirname(st.__file__)+'/static/index.html'
+
+# Open the file
+with open(path_ind, 'r') as index_file:
+    data=index_file.read()
+
+    # Check whether there is GA script
+    if len(re.findall('UA-', data))==0:
+
+        # Insert Script for Google Analytics
+        with open(path_ind, 'w') as index_file_f:
+
+            # The Google Analytics script should be pasted in the header of the HTML file
+            newdata=re.sub('<head>','<head>'+anlytcs_code,data)
+
+            index_file_f.write(newdata)
 st.set_page_config(page_title='Tradelyne', page_icon='📈', layout="wide",initial_sidebar_state='collapsed')
 st.markdown('<style>div.block-container{padding-left:0rem;}</style>', unsafe_allow_html=True)
 st.markdown("""
